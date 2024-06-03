1004beb83206a77aeb33a50e8d25d3a53a4d8cbd
unknown <eggitsadmin@bogarde.esrirw.com>
2024-06-03 15:51:12 +0200

    second build

--- src/Slider3.js
+++ src/Slider3.js
@@ -1,47 +1,59 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import Header from "./Header";
 import logo from "./img/logo3.png";
 
 const Slider3 = () => {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <div>
-
       <Header />
       
- 
       <div className={`absolute top-0 left-0 h-full bg-gray-200 w-64 p-10 transition-all duration-300 ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-   
         <button className="absolute top-4 right-4" onClick={toggleMenu}>
           &#10005;
         </button>
         
-    
-        <ul>
-         
-          <li>
-            <Link to="/slider1">Slider 1</Link>
-          </li>
-          <li>
-            <Link to="/slider2">Slider 2</Link>
-          </li>
-          <li>
-          
-            <div>
-              <button className="block">Dropdown</button>
-              <ul>
-                <li>
+        <ul className="space-y-4">
+        <li className="relative hover:bg-gray-300 p-2 ">
+            <div className="relative group">
+              <button className="block w-full text-left">Menu1</button>
+              <ul className="absolute left-full top-0 mt-0 hidden group-hover:block bg-gray-100 p-2 ">
+                <li className="hover:bg-gray-300 p-2 ">
                   <Link to="/submenu1">Submenu 1</Link>
                 </li>
-                <li>
+                <li className="hover:bg-gray-300 p-2 ">
+                  <Link to="/submenu2">Submenu 2</Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li className="relative hover:bg-gray-300 p-2 ">
+            <div className="relative group">
+              <button className="block w-full text-left">Menu2</button>
+              <ul className="absolute left-full top-0 mt-0 hidden group-hover:block bg-gray-100 p-2 ">
+                <li className="hover:bg-gray-300 p-2 ">
+                  <Link to="/submenu1">Submenu 1</Link>
+                </li>
+                <li className="hover:bg-gray-300 p-2">
+                  <Link to="/submenu2">Submenu 2</Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li className="relative hover:bg-gray-300 p-2">
+            <div className="relative group">
+              <button className="block w-full text-left">Menu3</button>
+              <ul className="absolute left-full top-0 mt-0 hidden group-hover:block bg-gray-100 p-2">
+                <li className="hover:bg-gray-300 p-2">
+                  <Link to="/submenu1">Submenu 1</Link>
+                </li>
+                <li className="hover:bg-gray-300 p-2">
                   <Link to="/submenu2">Submenu 2</Link>
                 </li>
               </ul>
@@ -50,13 +62,11 @@ const Slider3 = () => {
         </ul>
       </div>
 
-
       <button className="absolute top-4 left-4 z-10" onClick={toggleMenu}>
         &#9776;
       </button>
  
       <div className="absolute top-0 left-1/2 transform -translate-x-1/2">
-      
         <Link to="/">
           <img src={logo} alt="Logo" className="h-16 w-auto" />
         </Link>
